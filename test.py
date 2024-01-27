d93dfa1370ad6730b1180cf484bf6d18e976f5a0
saikrishna <saikrishna31120@gmail.com>
2024-01-27 19:49:50 +0530

    manual model download

--- test.py
+++ test.py
@@ -2,7 +2,18 @@ from paddleocr import PaddleOCR,draw_ocr
 # Paddleocr supports Chinese, English, French, German, Korean and Japanese.
 # You can set the parameter `lang` as `ch`, `en`, `fr`, `german`, `korean`, `japan`
 # to switch the language model in order.
-ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
+"""
+dowload links:
+    det_model = https://paddleocr.bj.bcebos.com/PP-OCRv3/english/en_PP-OCRv3_det_infer.tar
+    rec_model = https://paddleocr.bj.bcebos.com/PP-OCRv4/english/en_PP-OCRv4_rec_infer.tar
+    cls_path  = https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar
+
+extract the .tar files and place the folder in downloads
+"""
+
+
+#ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
+ocr = PaddleOCR(det_model_dir='downloads/en_PP-OCRv3_det_infer', rec_model_dir='downloads/en_PP-OCRv4_rec_infer', cls_model_dir='downloads/ch_ppocr_mobile_v2.0_cls_infer',use_angle_cls=True, lang='en')
 img_path = 'downloads/254.jpg'
 result = ocr.ocr(img_path, cls=True)
 for idx in range(len(result)):
